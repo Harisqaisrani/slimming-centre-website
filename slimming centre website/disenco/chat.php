f093c206b1d43c6a953c4c79d562fe6665ffe6ca
Harris Qaisrani <39429797+Harisqaisrani@users.noreply.github.com>
2023-11-04 18:38:41 +0500

    Add files via upload

---
+++ slimming centre website/disenco/chat.php
@@ -0,0 +1,8 @@
+<?php
+        $servername = "localhost";
+        $user_name = "root";
+        $password = "";
+        $database = "db_design";
+
+        $conn = new mysqli($servername,$user_name,$password,$database);
+?>
